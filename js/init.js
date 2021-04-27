5d4a7e08f22669bef668ec112100812b4a32c4e4
kan <root.kan.ss@gmail.com>
2021-04-27 15:07:46 -0400

    solucionado mini bug con el carrusel

--- js/init.js
+++ js/init.js
@@ -2,6 +2,11 @@
   $(function(){
     $('.sidenav').sidenav();
     $('.carousel').carousel({dist: 0,padding: 0,fullWidth: true,indicator: false,duration: 100,});
+    autoplay();
+    function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 2500);
+    }
     $('.materialboxed').materialbox();
     $('.parallax').parallax();
     $('select').formSelect();
@@ -13,10 +18,6 @@
       padding: 0,
       width: 0
     });
-    autoplay();
-    function autoplay() {
-      $('.carousel').carousel('next');
-      setTimeout(autoplay, 2500);
-    }
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
