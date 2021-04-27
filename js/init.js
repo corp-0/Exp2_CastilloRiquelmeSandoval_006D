a3cc317659b36aba19873c75fc6650b7bb36ea38
kan <root.kan.ss@gmail.com>
2021-04-26 23:35:46 -0400

    actualización de los scripts para los inputs

--- js/init.js
+++ js/init.js
@@ -5,10 +5,18 @@
     $('.materialboxed').materialbox();
     $('.parallax').parallax();
     $('select').formSelect();
+    $('select').material_select();
+    // for HTML5 "required" attribute
+    $("select[required]").css({
+      display: "inline",
+      height: 0,
+      padding: 0,
+      width: 0
+    });
     autoplay();
     function autoplay() {
-        $('.carousel').carousel('next');
-        setTimeout(autoplay, 2500);
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 2500);
     }
   }); // end of document ready
 })(jQuery); // end of jQuery name space
