a66ffd57292fba7239bf4edde965de2407092667
Gilles <43683714+corp-0@users.noreply.github.com>
2021-05-04 21:56:59 -0400

    Implementa modo oscuro en todas las páginas (#18)
    * implementación de darkmode con memoria
    
    * integra un gatillo para darkmode en las vistas

--- js/init.js
+++ js/init.js
@@ -1,12 +1,6 @@
 (function($){
   $(function(){
     $('.sidenav').sidenav();
-    $('.carousel').carousel({dist: 0,padding: 0,fullWidth: true,indicator: false,duration: 100,});
-    autoplay();
-    function autoplay() {
-      $('.carousel').carousel('next');
-      setTimeout(autoplay, 2500);
-    }
     $('.materialboxed').materialbox();
     $('.parallax').parallax();
     $('select').formSelect();
@@ -17,6 +11,34 @@
       padding: 0,
       width: 0
     });
+    $('.carousel').carousel({dist: 0,padding: 0,fullWidth: true,indicator: false,duration: 100,});
+    rememberColorMode();
+    carouselAutoPlay();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function carouselAutoPlay() {
+  $('.carousel').carousel('next');
+  setTimeout(carouselAutoPlay, 2500);
+}
+
+function rememberColorMode() {
+
+  const ClassMemory = [
+    ["html" , "ColorMode"] ,
+    ["html" , "TextSize"]
+  ] ;
+
+  for ( let [ Tag , MemoryName ] of ClassMemory ) {
+
+    let TagNotAlive = !(document.querySelector( Tag )) ;
+
+    let MemoryValue = window.localStorage.getItem( MemoryName ) ;
+
+    if ( MemoryValue || TagNotAlive ) {
+      document.querySelector( Tag ).classList.add( MemoryValue );
+    }
+  }
+
+}
